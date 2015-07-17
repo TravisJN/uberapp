d88c2913b7f7c186ea7c3658e3a12c548db5e772
travisjn <tdizzletrav@gmail.com>
2015-07-16 21:41:47 -0700

    Make GET request to uber api on server load

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Express server
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var app = express();
 var port = process.env.PORT || 3333;
@@ -8,5 +9,27 @@ var port = process.env.PORT || 3333;
 app.use('/', express.static(__dirname + '/client'));
 
 app.use(bodyParser.json());
+
+var parameters = {
+  'server_token': 'INSERT_SERVER_TOKEN_HERE',
+  'latitude': 37.775818,
+  'longitude': -122.418028
+};
+
+request({
+  url: 'https://sandbox-api.uber.com/v1/products',
+  method: 'GET',
+  qs: {
+    server_token: 'wsHKtggSl-GeCLrC5f7vfStaRdEOZh-qEui6Mxuo',
+    latitude: 37.775818,
+    longitude: -122.418028
+  },
+  headers: {
+    'Content-Type': 'application/JSON',
+  }
+}, function(error, res, body) {
+  console.log('body:', body);
+  console.log('error:', error);
+});
 
 app.listen(port, function() { console.log('listening on port ' + port + '...')});
